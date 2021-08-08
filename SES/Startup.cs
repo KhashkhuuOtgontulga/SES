cec4d6ef129b5d3d5c998d3c9cb8821c94f7b825
khashkhuu <khashotgo@gmail.com>
2021-08-08 16:10:57 -0500

    * SES.csproj: * _Layout.cshtml:
    * Startup.cs: Edit operation working

--- SES/Startup.cs
+++ SES/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RazorPages.Services;
 
 namespace SES
 {
@@ -24,6 +25,7 @@ namespace SES
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IUserRepository, MockUserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
